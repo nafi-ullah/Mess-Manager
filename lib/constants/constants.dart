2c39d7c295bd922f812f9f4e93c51fe4c138e344
nafi-ullah <shafinnafiullah@gmail.com>
2024-01-17 17:50:28 +0600

    bazarshow added

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 const kBackgroundColor =  Color(0xFF262b31);
 const kPrimaryColor = Color.fromRGBO(223, 37, 60, 1);
 const ksecondaryHeaderColor = Color(0xFF151a1d);
-String uri= 'http://192.168.0.145:3000';
+String uri= 'http://192.168.0.114:3000';
 
  const appBarGradient = LinearGradient(
   colors: [
---
+++ lib/screens/bazarShow.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:mess_app/constants/constants.dart';
+
+class BazarShow extends StatefulWidget {
+  const BazarShow({super.key});
+
+  @override
+  State<BazarShow> createState() => _BazarShowState();
+}
+
+class _BazarShowState extends State<BazarShow> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar:  PreferredSize(
+        preferredSize: const Size.fromHeight(50),
+        child: AppBar(
+          flexibleSpace: Container(
+            decoration: const BoxDecoration(
+              color: Colors.black,
+            ),
+          ),
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                  height: 40,
+                  alignment: Alignment.topLeft,
+                  child: const Text("Bazar Page", style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                  ),)
+              ),
+              Container(
+                padding: const EdgeInsets.only(left: 15, right: 15),
+                child: const Row(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(right: 15),
+                      child: Icon(Icons.notifications_outlined),
+                    ),
+                    Icon(
+                      Icons.search,
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+      body: Column(
+        children: [
+          Text("History"),
+
+        ],
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+          onPressed: (){},
+          elevation: 0.0,
+          label: const Text("Add"),
+          foregroundColor: Colors.white,
+          icon: const Icon(Icons.add_shopping_cart_sharp),
+          backgroundColor: kPrimaryColor,
+      ),
+    );
+  }
+}
